b2a6b58c33cfa641cd49fc575de701a03fee7df5
Vince Andrew Pimentel <contact@vincep.me>
2019-01-12 01:28:28 +0000

    Automatically backed up by Learn

--- db/migrate/20180314210852_create_users.rb
+++ db/migrate/20180314210852_create_users.rb
@@ -1,7 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def up
   end
-  
+
   def down
+  end
+
+  def change
+    create_table :users do |c|
+      t.string :username
+      t.string :password_digest
+    end
   end
 end
